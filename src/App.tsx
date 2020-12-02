cb98a257a25c022a2a6536539cb50488f3d00b9d
ogbeche77 <ogbeche77@yahoo.com>
2020-12-02 12:09:29 +0100

    [#3} implemented todo app functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import TodoList from "./TodoList";
-//import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 //import * as ReactDOM from "react-dom";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Lets go Shopping" }];
   return (
     <div className="App">
+      {/* <NewTodo /> */}
       <TodoList items={todos} />
     </div>
   );
